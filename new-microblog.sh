be719fa764a696779fc62e91bcda3ee8fb29958f
cblte <cbrueggenolte@me.com>
2021-05-21 00:05:59 +0200

    Create microblog script

---
+++ new-microblog.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# create a new microblog post based on date and time
+FOLDER="$(date +'%Y%m%d')"
+FILENAME="$(date +'%Y%m%d-%H%M%S')"
+
+hugo new microblog/$FOLDER/$FILENAME.md
+cd content/microblog/$FOLDER
+codium $FILENAME.md
